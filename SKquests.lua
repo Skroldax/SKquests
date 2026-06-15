662bae95a316ab79f851db91a66669bc437e7228
Kevinthoo <k.villaloboscuevas@uandresbello.edu>
2026-06-14 23:16:51 -0400

    docs: add copyright and license headers to all core code files

--- SKquests.lua
+++ SKquests.lua
@@ -1,3 +1,18 @@
+--[[
+    SKquests - WoW Leveling Guide & Quest Database Explorer Addon
+    Copyright (c) 2026 Skroldax. All rights reserved.
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+--]]
+
 -- SKquests addon core (WoW 3.3.5a compatible)
 local addonName = "SKuests"
 local altAddon  = "SKquests"
